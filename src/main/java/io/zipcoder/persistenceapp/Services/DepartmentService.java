48e3dc5e51009976adda7a4cf1b41a56cddbdd7d
Chris Allen <callen7908@yahoo.com>
2021-04-16 21:46:54 -0400

    Services done

--- src/main/java/io/zipcoder/persistenceapp/Services/DepartmentService.java
+++ src/main/java/io/zipcoder/persistenceapp/Services/DepartmentService.java
@@ -1,7 +1,42 @@
 package io.zipcoder.persistenceapp.Services;
 
+import io.zipcoder.persistenceapp.Models.Department;
+import io.zipcoder.persistenceapp.Models.Employee;
+import io.zipcoder.persistenceapp.Repositories.DepartmentRepository;
+import io.zipcoder.persistenceapp.Repositories.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DepartmentService {
+
+    private DepartmentRepository repository;
+
+    public DepartmentService(DepartmentRepository departmentRepository){
+        this.repository = departmentRepository;
+    }
+
+    public Iterable<Department> index(){
+        return repository.findAll();
+    }
+
+    public Department show(Long id){
+        return repository.findOne(id);
+    }
+
+    public Department create(Department department){
+        return repository.save(department);
+    }
+
+    public Department update(Long id,Department department){
+        Department originalDepartment = repository.findOne(id);
+        originalDepartment.setDepartmentNumber(department.getDepartmentNumber());
+        originalDepartment.setDepartmentName(department.getDepartmentName());
+        originalDepartment.setDepartmentManager(department.getDepartmentManager());
+        return repository.save(originalDepartment);
+    }
+
+    public Boolean delete(Long id){
+        repository.delete(id);
+        return true;
+    }
 }
